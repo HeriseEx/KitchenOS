31ca354248886ac7c17ff2b55e13f21d83f47f33
herise <710876917@qq.com>
2026-01-24 14:22:25 +0800

    build: Rebuild web with Android Chrome speech fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -22,16 +22,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "770b59a779606b529226dc6e1a97e912",
+"flutter_bootstrap.js": "cb4387c72c2afccaf780ad6c992055f5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "534ef6af91584e0d787c9b569cbf35f8",
-"/": "534ef6af91584e0d787c9b569cbf35f8",
-"main.dart.js": "b913cb82775b3e375cdeaf649c6a88fb",
+"index.html": "dd7478dc4002f5b7b6f8aad799789083",
+"/": "dd7478dc4002f5b7b6f8aad799789083",
+"main.dart.js": "608dacdfe6e0f118437558d252d3c640",
 "manifest.json": "758606c67a2f7bb302d8a7591fc6c66e",
-"vercel.json": "e08633c79448542c130949a5ab82051d",
+"vercel.json": "d9a7ce65ae15c9b56ba55be0407d9e30",
 "version.json": "ce7636a80668ac4d364ec248d0985e4b"};
 // The application shell files that are downloaded before a service worker can
 // start.
