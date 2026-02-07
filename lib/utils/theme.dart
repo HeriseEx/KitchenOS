b6988595399a1fc8a8ee1e31b3057e92a230ef16
herise <710876917@qq.com>
2026-02-07 18:44:28 +0800

    fix: replace CardTheme with CardThemeData for compatibility

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -139,7 +139,7 @@ class AppTheme {
         ),
       ),
 
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 0,
         color: surfaceColor,
         margin: EdgeInsets.zero,
@@ -227,7 +227,7 @@ class AppTheme {
         foregroundColor: Colors.white,
         systemOverlayStyle: SystemUiOverlayStyle.light,
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 0,
         color: darkSurface,
         shape: RoundedRectangleBorder(
